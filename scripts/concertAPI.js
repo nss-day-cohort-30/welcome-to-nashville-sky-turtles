7f118bb4c62c42b52c38d9d99b27173583c129e0
katerina <katerina@williams-mbp.attlocal.net>
2019-01-24 17:49:17 -0600

    add some functionality to button. IT now adds everything to the DOM when clicked

--- scripts/concertAPI.js
+++ scripts/concertAPI.js
@@ -1,11 +1,24 @@
-fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=312&apikey=DW1OYVAwf6ati7WVNtEdLEku4AHB2pF0")
+document.querySelector("#concertButton").addEventListener("click", function () {
+    pingAPI()
+}) 
+
+pingAPI(
+
+)
+
+
+
+function pingAPI()  {
+    
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=312&apikey=DW1OYVAwf6ati7WVNtEdLEku4AHB2pF0")
     .then(events => events.json())
     .then(parsedEvents => {
         // console.table(parsedEvents)
         console.log(parsedEvents._embedded.events)
-        
+
         parsedEvents._embedded.events.forEach(events => {
             console.log(events.classifications[0].genre.name)
+            console.log(events._embedded.venues[0].name)
             const eventsonDOM = showEvent(events)
             addtoDOM(eventsonDOM)
         });
@@ -14,10 +27,10 @@ fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=
 const showEvent = events => {
     return `
          <div class="eventsContainer">
-             <h2 class = "eventsName" >${events.name}</h2>
-             <p class = "eventsGenre" >${events.classifications[0].genre.name} </p>
-             <p class = "eventsVenueName" >${events._embedded.venues.name}</p>
-             
+             <h2 class = "eventsName">${events.name}</h2>
+             <p class = "eventsGenre">  Music Genre: ${events.classifications[0].genre.name} </p>
+             <p class = "eventsVenueName"> Event Date: ${events.dates.start.localDate}</p>
+              <p class = "eventsVenueName"> Event Venue: ${events._embedded.venues[0].name}</p>
          </div>
          `
 }
@@ -25,8 +38,6 @@ const showEvent = events => {
 const addtoDOM = eventsonDOM => {
     document.querySelector("#results--container").innerHTML += eventsonDOM
 }
+}
 
 
-
-//  <p class = "eventsVenueName" >${events.venues.name}</p>
-/* <p class = "eventsGenre" >${events.classifications.genre} </p> */
