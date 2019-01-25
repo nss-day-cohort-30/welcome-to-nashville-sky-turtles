c585f54e998d309d27c920d903378c6c523aeafc
James Farrell <jamesfarrellmusic@gmail.com>
2019-01-25 09:29:56 -0600

    added emojis

--- scripts/restaurantsapi.js
+++ scripts/restaurantsapi.js
@@ -3,35 +3,40 @@ let containerEl = document.querySelector("#results--container")
 document.getElementById("request--food").addEventListener("click", function () {
     containerEl.innerHTML = ""
     search = document.querySelector(".search--food").value
-    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${search}&apikey=32e7ff95eabcfcc0019eb633902941bc`)
+    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${search}&sort=rating&apikey=32e7ff95eabcfcc0019eb633902941bc`)
         .then(restaurants => restaurants.json())
         .then(parsedRestaurants => {
+            if (parsedRestaurants.restaurants.length > 0) {
             parsedRestaurants.restaurants.forEach(restaurant => {
                 let htmlElement = htmlFactory(restaurant)
                 postToResultsDom(htmlElement)
             });
+        } else {
+            containerEl.innerHTML += "<h2><em>No results found</em> 🤷‍♀️</h2>"
+        }
         })
 })
 
 let htmlFactory = (restaurant) => {
+    let id = restaurant.restaurant.id
     let name = restaurant.restaurant.name
     let cuisines = restaurant.restaurant.cuisines
     let address = restaurant.restaurant.location.address
     let rating = restaurant.restaurant.user_rating.aggregate_rating
 
     return `
-    <div>    
+    <div class="restaurant--results--card">    
         <h3>${name}</h3>
         <p>Rating: ${rating}</p>
         <p>Cuisine: ${cuisines}</p>
         <p>Address: ${address}</p>
-        <button id="${name}">SAVE</button>
+        <button id="${id}">SAVE</button>
     </div>
     `
 }
 
 let itineraryHTML = () => {
-    return `<div>Restaurant: ${name}</div>`
+    return `<div class="restaurant--itinerary--card">Restaurant: ${name}</div>`
 }
 
 let postToResultsDom = (htmlElement) => {
