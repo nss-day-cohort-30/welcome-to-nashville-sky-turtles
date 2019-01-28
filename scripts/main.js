5f7e0e192647115f16effacfacb4393b68e805c6
James Farrell <jamesfarrellmusic@gmail.com>
2019-01-28 15:40:39 -0600

    created function to clear divs

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+clearDivs = () => {
+    document.querySelector("#parksContainer").innerHTML=""
+    document.querySelector("#restaurantItinerary").innerHTML=""
+    document.querySelector("#meetupItinerary").innerHTML=""
+    document.querySelector("#concertItinerary").innerHTML=""
+}
