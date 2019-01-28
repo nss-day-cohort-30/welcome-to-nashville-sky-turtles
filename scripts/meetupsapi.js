c63fda6560b3472a2dc028ca3e558ba25c3dd4d5
Janet Woods <janetnwoods@gmail.com>
2019-01-28 11:55:33 -0600

    showing events, not saving to itinerary yet

--- scripts/meetupsapi.js
+++ scripts/meetupsapi.js
@@ -1,6 +1,6 @@
 const meetupSearch = document.querySelector("#searchform--meetups")
 const place4MeetupResults = document.querySelector("#results--container")
-let allEvents=[]
+let allEvents = []
 fetch(`https://www.eventbriteapi.com/v3/events/search/?q=meetup_nashville&token=OEA3462VUJJRZA57Z7GC`, {
     headers: {
         "Authorization": "Bearer OEA3462VUJJRZA57Z7GC",
@@ -9,11 +9,32 @@ fetch(`https://www.eventbriteapi.com/v3/events/search/?q=meetup_nashville&token=
 })
 .then(events => events.json())
 .then(parsedEvents => {
-
+    
     parsedEvents.events.forEach(event => {
         console.log(event.name)
+        
+        /// variable to list meetups to DOM
+        const meetupsToDom = meetupBuilder(event)
+        addToDom(meetupsToDom)
+        
     });
-
-
-
+    
+    
 })
+
+const meetupBuilder = event => {
+    return `
+        <section name="meetup--section" id="meetup--article"> 
+        <p>${event.name.text} </p> 
+        
+        <button id="${event.id}">Save to itinerary</button>
+       
+        </section>
+        `
+}
+
+addToDom = (meetupsToDom) => {
+    document.querySelector("#results--container").innerHTML += meetupsToDom;
+
+}
+
