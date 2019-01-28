e0d43ce93ee7dfcbf4e59fe31d13857a9f3a2417
Janet Woods <janetnwoods@gmail.com>
2019-01-28 13:40:03 -0600

    pulling a list, searching. not saving yet

--- scripts/meetupsapi.js
+++ scripts/meetupsapi.js
@@ -1,40 +1,50 @@
 const meetupSearch = document.querySelector("#searchform--meetups")
 const place4MeetupResults = document.querySelector("#results--container")
 let allEvents = []
-fetch(`https://www.eventbriteapi.com/v3/events/search/?q=meetup_nashville&token=OEA3462VUJJRZA57Z7GC`, {
-    headers: {
-        "Authorization": "Bearer OEA3462VUJJRZA57Z7GC",
-        "Accept": "application/json"
-    },
-})
-.then(events => events.json())
-.then(parsedEvents => {
-    
-    parsedEvents.events.forEach(event => {
-        console.log(event.name)
-        
-        /// variable to list meetups to DOM
-        const meetupsToDom = meetupBuilder(event)
-        addToDom(meetupsToDom)
-        
-    });
-    
-    
-})
 
 const meetupBuilder = event => {
     return `
-        <section name="meetup--section" id="meetup--article"> 
-        <p>${event.name.text} </p> 
-        
-        <button id="${event.id}">Save to itinerary</button>
-       
-        </section>
-        `
+    <section name="meetup--section" id="meetup--article"> 
+    <p>${event.name.text} </p> 
+    
+    <button id="btn-${event.id}">Save to itinerary</button>
+    
+    </section>
+    `
 }
 
 addToDom = (meetupsToDom) => {
     document.querySelector("#results--container").innerHTML += meetupsToDom;
-
+    
 }
 
+
+document.querySelector("#request--meetups").addEventListener("click", function(){
+    searchField = document.querySelector(".search--meetups").value 
+    console.log("i pushed a button")
+    console.log("searchfield", searchField)
+
+
+    document.querySelector("#results--container").innerHTML = ""
+    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_${searchField}&token=OEA3462VUJJRZA57Z7GC`, {
+        headers: {
+            "Authorization": "Bearer OEA3462VUJJRZA57Z7GC",
+            "Accept": "application/json"
+        }
+    })
+    .then(events => events.json())
+    .then(parsedEvents => {
+        console.log(parsedEvents)
+        
+        parsedEvents.events.forEach(event => {
+            
+            /// variable to list meetups to DOM
+            const meetupsToDom = meetupBuilder(event)
+            addToDom(meetupsToDom)
+            
+        });
+        
+        
+    })
+    
+})
