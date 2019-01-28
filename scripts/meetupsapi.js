14906351075ec025e925a92f0ed104ad92778d26
Janet Woods <janetnwoods@gmail.com>
2019-01-28 14:09:05 -0600

    here it is

--- scripts/meetupsapi.js
+++ scripts/meetupsapi.js
@@ -2,25 +2,10 @@ const meetupSearch = document.querySelector("#searchform--meetups")
 const place4MeetupResults = document.querySelector("#results--container")
 let allEvents = []
 
-const meetupBuilder = event => {
-    return `
-    <section name="meetup--section" id="meetup--article"> 
-    <p>${event.name.text} </p> 
-    
-    <button id="btn-${event.id}">Save to itinerary</button>
-    
-    </section>
-    `
-}
-
-addToDom = (meetupsToDom) => {
-    document.querySelector("#results--container").innerHTML += meetupsToDom;
-    
-}
 
 
-document.querySelector("#request--meetups").addEventListener("click", function(){
-    searchField = document.querySelector(".search--meetups").value 
+document.querySelector("#request--meetups").addEventListener("click", function () {
+    searchField = document.querySelector(".search--meetups").value
     console.log("i pushed a button")
     console.log("searchfield", searchField)
 
@@ -32,19 +17,58 @@ document.querySelector("#request--meetups").addEventListener("click", function()
             "Accept": "application/json"
         }
     })
-    .then(events => events.json())
-    .then(parsedEvents => {
-        console.log(parsedEvents)
-        
-        parsedEvents.events.forEach(event => {
-            
-            /// variable to list meetups to DOM
-            const meetupsToDom = meetupBuilder(event)
-            addToDom(meetupsToDom)
-            
-        });
-        
-        
-    })
-    
+        .then(events => events.json())
+        .then(parsedEvents => {
+            console.log(parsedEvents)
+
+            parsedEvents.events.forEach(event => {
+
+                /// variable to list meetups to DOM
+                const meetupsToDom = meetupBuilder(event)
+                addToDom(meetupsToDom)
+
+            });
+
+
+        })
+
 })
+
+
+const meetupBuilder = event => {
+    return `
+    <section name="meetup--section" id="meetup--article"> 
+    <p>${event.name.text} </p> 
+    
+    <button id="btn-${event.id}">Save to itinerary</button>
+    
+    </section>
+    `
+}
+const selectedMeetupBuilder = (event) => {
+    return `
+    <section name="meetup--section" id="meetup--article"> 
+    <p>${event.name.text} </p> 
+    <p>${event.start}</p>
+    
+    </section>
+    `
+}
+
+
+addToDom = (meetupsToDom) => {
+    document.querySelector("#results--container").innerHTML += meetupsToDom;
+
+
+}
+const theSelectedBtn = document.querySelector(`#btn-${event.event.id}`)
+
+theSelectedBtn.addEventListener("click", saveToItinerary)
+
+saveToItinerary = (theSelectedBtn) => {
+
+    document.querySelector("#meetupItinerary").innerHTML = ""
+    if (theSelectedBtn.split("-")[1] === event.id)
+        selectedMeetupBuilder(this.event)
+    document.querySelector("#meetupItinerary").innerHTML += meetupsToDom;
+}
